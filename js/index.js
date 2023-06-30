f5e65292aca6c32dc8540cfc86263c969ad131b1
kula <60266047.okura.cgp@gmail.com>
2023-06-30 12:39:14 +0900

    [fix]ファイル名の変更・スクリプト修正
    indexをメインシーンに変更
    一部画像の追加
    メインシーンの修正

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
 import {
   getAuth,
+  setPersistence,
   signInWithEmailAndPassword,
+  browserSessionPersistence,
 } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
 
 const firebaseConfig = {
@@ -17,34 +19,9 @@ const firebaseConfig = {
 initializeApp(firebaseConfig);
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-let login_button = document.getElementById("login-button");
-let new_button = document.getElementById("new-button");
 
-//ログイン処理
-document.addEventListener("DOMContentLoaded", function () {
-  login_button.addEventListener(
-    "click",
-    function () {
-      var mailAddress = document.getElementById("login-mail").value;
-      var password = document.getElementById("login-pass").value;
-      document.getElementById("login-error-text").innerText = "";
-
-      signInWithEmailAndPassword(auth, mailAddress, password)
-        .then((userCredential) => {
-          // Signed in
-          const user = userCredential.user;
-          location.href = "mainmenu.html";
-        })
-        .catch((error) => {
-          document.getElementById("login-error-text").innerText =
-            "メールアドレスまたはパスワードが正しくありません。";
-        });
-    },
-    false
-  );
+firebaseApp.fireauth.onAuthStateChanged((user) => {
+  if (!user) {
+    window.location.replace("login.html");
+  }
 });
-
-// 新規作成ページに遷移
-new_button.onclick = function () {
-  location.href = "new_account.html";
-};
