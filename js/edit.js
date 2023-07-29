bdb369c38a446dff11d15eb92b5627baba0c9cd9
sgoflt <135018476+sgoflt@users.noreply.github.com>
2023-07-29 11:56:20 +0900

    [fix]地獄

--- js/edit.js
+++ js/edit.js
@@ -8,12 +8,8 @@ import {
     getFirestore,
     doc,
     setDoc,
-    addDoc,
     getDoc,
-    getDocs,
-    collection,
-    query,
-    where,
+
 } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
 
 //情報設定
@@ -31,7 +27,6 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 
 //htmlと連携
-let back = document.getElementById("back_button");
 let logout = document.getElementById("logout");
 let logoicon = document.getElementById("logoicon");
 let exiticon = document.getElementById("exiticon");
@@ -49,72 +44,89 @@ document.addEventListener("DOMContentLoaded", async function () {
     const url = new URL(window.location.href);
     const param = url.searchParams;
     const doc_id = param.get("docid");
+    const userId = user.uid;
 
 
-    if (user) {
-      app;
-      const user = auth.currentUser;
-      const email = user.email;
-      document.getElementById("user_mail").innerText = email;
-      //document.getElementById("user_mail-sp").innerText = email;
-      // ↑ HTMLを見るにPC版のみになっているから「user_mail-sp」がHTML内にない
-
-      
-
-      // ドキュメントIDからデータを取得
-      try {
-        const docRef = doc(db, "data", doc_id);
-        const docSnap = await getDoc(docRef);
-        if (docSnap.exists) {
-          const data = docSnap.data();
-
-          // HTMLから取得
-          const textarea = document.getElementById("textarea");
-          const word = document.getElementById("inputlength");
-          const time = document.getElementById("inputtime");
-          const word_i = document.getElementsByClassName("mojisu")[0];
-          const time_i = document.getElementsByClassName("time")[0];
-          const title = document.getElementsByClassName("genko_title")[0];
-
-          // 反映
-          if (data.title == undefined || data.title == null) {
-            title.value = "無題";
-          } else {
-            title.value = data.title;
-          }
-          if (data.text == undefined || data.text == null) {
-            textarea.innerHTML = "本文";
-          } else {
-            textarea.innerHTML = data.text;
-          }
-          if (data.word_ideal == undefined || data.word_ideal == null) {
-            word_i.value = "0";
-          } else {
-            word_i.value = data.word_ideal;
-          }
-          if (data.word_input == undefined || data.word_input == null) {
-            word.innerText = "0";
-          } else {
-            word.innerText = data.word_input;
-          }
-          if (data.time_ideal == undefined || data.time_ideal == null) {
-            time_i.value = "0";
-          } else {
-            time_i.value = data.time_ideal;
-          }
-          if (data.time_input == undefined || data.time_input == null) {
-            time.innerText = "0";
-          } else {
-            time.innerText = data.time_input;
-          }
-        } else {
-          console.log("No such document!");
-        }
-      } catch (error) {
-        console.log("Error getting document:", error);
-      }
-    }
+      if (user) {
         
+        const user = auth.currentUser;
+        const email = user.email;
+        document.getElementById("user_mail").innerText = email;
+        //document.getElementById("user_mail-sp").innerText = email;
+        // ↑ HTMLを見るにPC版のみになっているから「user_mail-sp」がHTML内にない
+
+        
+
+        // ドキュメントIDからデータを取得
+        try {
+          const docRef = doc(db, "data", doc_id);
+          const docSnap = await getDoc(docRef);
+          if (docSnap.exists) {
+            const data = docSnap.data();
+            if (data.userId === userId) {
+              console.log("data.userId:"+data.userId, "userId:"+ userId);
+              // HTMLから取得
+              const textarea = document.getElementById("textarea");
+              const word = document.getElementById("inputlength");
+              const time = document.getElementById("inputtime");
+              const word_i = document.getElementsByClassName("mojisu")[0];
+              const time_i = document.getElementsByClassName("time")[0];
+              const title = document.getElementsByClassName("genko_title")[0];
+              // 反映
+              if (data.title == undefined || data.title == null) {
+                title.value = "無題";
+              } else {
+                title.value = data.title;
+              }
+              if (data.text == undefined || data.text == null) {
+                textarea.innerHTML = "本文";
+              } else {
+                textarea.innerHTML = data.text;
+              }
+              if (data.word_ideal == undefined || data.word_ideal == null) {
+                word_i.value = "0";
+              } else {
+                word_i.value = data.word_ideal;
+              }
+              if (data.word_input == undefined || data.word_input == null) {
+                word.innerText = "0";
+              } else {
+                word.innerText = data.word_input;
+              }
+              if (data.time_ideal == undefined || data.time_ideal == null) {
+                time_i.value = "0";
+              } else {
+                time_i.value = data.time_ideal;
+              }
+              if (data.time_input == undefined || data.time_input == null) {
+                time.innerText = "0";
+              } else {
+                time.innerText = data.time_input;
+              }
+              //「はい」をクリックしたときにデータをFirebaseに更新して保存
+              const yesButton = document.getElementById("yesButton");
+              yesButton.addEventListener('click', () => {
+                  // Firebaseにデータを保存
+                  updateDataToFirebase();
+                  // モーダルを閉じる
+                  mask.classList.add('hidden');
+                  modal.classList.add('hidden');
+              });
+            } else {
+              alert("ユーザーの UIDが一致していません。ログアウトしてやり直してください。")
+            }
+          } else {
+            console.log("No such document!");
+          }
+        } catch (error) {
+          console.log("Error getting document:", error);
+        }
+      } else {
+        // ログインしていない場合の処理
+        alert("ログインしてください。")
+        location.href = "login.html";
+      }
+      
       // Firebase Firestoreにデータを更新して保存する
         const updateDataToFirebase = () => { 
             const idealNumber = document.querySelector('.mojisu').value;
@@ -143,20 +155,11 @@ document.addEventListener("DOMContentLoaded", async function () {
             .catch((error) => {
                 alert("データの保存中にエラーが発生しました: " + error);
             });
-        };
+          }
 
-        //「はい」をクリックしたときにデータをFirebaseに更新して保存
-        const yesButton = document.getElementById("yesButton");
-        yesButton.addEventListener('click', () => {
-          
-          // Firebaseにデータを保存
-          updateDataToFirebase();
-          
-          // モーダルを閉じる
-          mask.classList.add('hidden');
-          modal.classList.add('hidden');
         
-    });
+          
+        
 
     // 「いいえ」をクリックしたときにモーダルを閉じる
     const noButton = document.getElementById("noButton");
@@ -181,48 +184,49 @@ document.addEventListener("DOMContentLoaded", async function () {
     });
 
 
+ 
+});
+
+  // ログアウトボタンを押下
+  logout.addEventListener("click", () => {
+    signOut(auth)
+      .then(() => {
+        location.href = "login.html";
+      })
+      .catch((error) => {
+        console.log(`ログアウト時にエラーが発生しました (${error})`);
+      });
   });
-});
-
-// ログアウトボタンを押下
-logout.addEventListener("click", () => {
-  signOut(auth)
-    .then(() => {
-      location.href = "login.html";
-    })
-    .catch((error) => {
-      console.log(`ログアウト時にエラーが発生しました (${error})`);
-    });
-});
 
 
 
-//保存ボタン
-const savebtn_sm = document.getElementById("saveicon_2");
-const mask_sm = document.getElementById("mask_sm");
-const modal_sm = document.getElementById("modal_sm");
+  //保存ボタン
+  const savebtn_sm = document.getElementById("saveicon_2");
+  const mask_sm = document.getElementById("mask_sm");
+  const modal_sm = document.getElementById("modal_sm");
 
-savebtn_sm.addEventListener('click', () => {
-    mask_sm.classList.remove('hidden');
-    modal_sm.classList.remove('hidden');
-});
+  savebtn_sm.addEventListener('click', () => {
+      mask_sm.classList.remove('hidden');
+      modal_sm.classList.remove('hidden');
+  });
 
 
 
 
-// ロゴをクリックするとメニュー画面に移動
-logoicon.addEventListener("click", () => {
-  location.href = "index.html";
-});
-// exitボタンをクリックするとメニュー画面に移動
-exiticon.addEventListener("click", () => {
-  location.href = "newlist.html";
-});
-// homeボタンをクリックするとメニュー画面に移動
-home.addEventListener("click", () => {
-  location.href = "index.html";
-});
-// ひとつ前に戻るボタンを押すとメニュー画面に移動
-back_button.addEventListener("click", () => {
-  location.href = "newlist.html";
-});
+  // ロゴをクリックするとメニュー画面に移動
+  logoicon.addEventListener("click", () => {
+    location.href = "index.html";
+  });
+  // exitボタンをクリックするとメニュー画面に移動
+  exiticon.addEventListener("click", () => {
+    location.href = "newlist.html";
+  });
+  // homeボタンをクリックするとメニュー画面に移動
+  home.addEventListener("click", () => {
+    location.href = "index.html";
+  });
+  // ひとつ前に戻るボタンを押すとメニュー画面に移動
+  back_button.addEventListener("click", () => {
+    location.href = "newlist.html";
+  });
+ });
