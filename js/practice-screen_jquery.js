61e38ffca0df280ed8ff39613975e183d45b6e35
kula <60266047.okura.cgp@gmail.com>
2023-08-27 13:03:40 +0900

    [fix]ハンバーガーメニュー実装(スマホ)
    ユーザーボタンをハンバーガーメニューに変更し、ユーザーボタンとフルスクリーンボタンを収納する仕様変更

--- js/practice-screen_jquery.js
+++ js/practice-screen_jquery.js
@@ -1,25 +1,73 @@
+// ハンバーガーボタン
+$(".hmb-img-sp").click(function () {
+  if ($(".hmb-img-sp").hasClass("opened")) {
+    $(".hmb-img-sp").removeClass("opened");
+    if (window.matchMedia("(max-width: 300px)").matches) {
+      $(".user_button-sp").animate(
+        { marginTop: "75px" },
+        {
+          duration: 300,
+          complete: function () {
+            $(".user-sp").css({ cssText: "display: none !important;" });
+          },
+        }
+      );
+      $("#fullicon-sp").animate(
+        { marginTop: "75px" },
+        {
+          duration: 300,
+          complete: function () {
+            $("#fullicon-sp").css({ cssText: "display: none !important;" });
+          },
+        }
+      );
+    } else {
+      $(".user_button-sp").animate(
+        { marginTop: "55px" },
+        {
+          duration: 300,
+          complete: function () {
+            $(".user-sp").css({ cssText: "display: none !important;" });
+          },
+        }
+      );
+      $("#fullicon-sp").animate(
+        { marginTop: "55px" },
+        {
+          duration: 300,
+          complete: function () {
+            $("#fullicon-sp").css({ cssText: "display: none !important;" });
+          },
+        }
+      );
+    }
+  } else {
+    $(".hmb-img-sp").toggleClass("opened");
+    $(".user-sp").css({ cssText: "display: block !important;" });
+    $("#fullicon-sp").css({ cssText: "display: block !important;" });
+    $(".user_button-sp").animate({ marginTop: "150px" });
+    $("#fullicon-sp").animate({ marginTop: "230px" });
+  }
+});
+
+// ユーザーボタン
 $(".user-img").click(function () {
-  //ボタンがクリックされたら
-  $(this).toggleClass("active"); //ボタン自身に activeクラスを付与し
-  $("#user-wrap").toggleClass("panelactive"); //ナビゲーションにpanelactiveクラスを付与
+  $(this).toggleClass("active");
+  $("#user-wrap").toggleClass("panelactive");
 });
-
-$("#user-wrap p").click(function () {
-  //ナビゲーションのリンクがクリックされたら
-  $(".openbtn1").removeClass("active"); //ボタンの activeクラスを除去し
-  $("#user-wrap").removeClass("panelactive"); //ナビゲーションのpanelactiveクラスも除去
-});
-
 $(".user-img-sp").click(function () {
-  //ボタンがクリックされたら
-  $(this).toggleClass("active"); //ボタン自身に activeクラスを付与し
-  $("#user-wrap-sp").toggleClass("panelactive"); //ナビゲーションにpanelactiveクラスを付与
+  $(this).toggleClass("active");
+  $("#user-wrap-sp").toggleClass("panelactive");
 });
 
+// ユーザータブ
+$("#user-wrap p").click(function () {
+  $(".openbtn1").removeClass("active");
+  $("#user-wrap").removeClass("panelactive");
+});
 $("#user-wrap-sp p").click(function () {
-  //ナビゲーションのリンクがクリックされたら
-  $(".openbtn1").removeClass("active"); //ボタンの activeクラスを除去し
-  $("#user-wrap-sp").removeClass("panelactive"); //ナビゲーションのpanelactiveクラスも除去
+  $(".openbtn1").removeClass("active");
+  $("#user-wrap-sp").removeClass("panelactive");
 });
 
 // メッセージのフェードイン・フェードアウト
