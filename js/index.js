86e080bd83c1f02ebde1d9b9682bc2515e183dc5
kula <60266047.okura.cgp@gmail.com>
2023-08-29 12:47:05 +0900

    [fix]クレジットの実装
    ・index.htmlにクレジットを実装
    ・デプロイやり直し

--- js/index.js
+++ js/index.js
@@ -22,6 +22,8 @@ const auth = getAuth(app);
 let logout = document.getElementById("logout");
 let create = document.getElementById("create_menu");
 let practice = document.getElementById("practice_menu");
+let credit = document.getElementById("credit");
+let credit_button = document.getElementById("credit_button");
 
 // 戻るボタンを実質的に無効化
 history.replaceState(null, null, null);
@@ -63,4 +65,81 @@ create.addEventListener("click", () => {
 // 発表練習ボタンをクリック
 practice.addEventListener("click", () => {
   location.href = "practicelist.html";
+});
+
+credit.addEventListener("click", () => {
+  Swal.fire({
+    title: "作業担当",
+    html:
+      "<企画・立案><br />大倉、杉尾、杉崎" +
+      "<br />" +
+      "- UIデザイン：主担当...杉尾" +
+      "<br />" +
+      "　　　　　　　原稿ダウンロード・フルスクリーン表示ボタン・" +
+      "<br />" +
+      "　　　　　　　マイク停止ボタン・初期化ボタン・クレジットボタン...大倉" +
+      "<br />" +
+      "- ログイン・アカウント登録画面：大倉 - メニュー画面：大倉" +
+      "<br />" +
+      "- 原稿リスト画面(新規作成・編集用/発表練習用)：大倉" +
+      "<br />" +
+      "- 原稿新規作成画面：主担当...杉尾" +
+      "<br />" +
+      "　　　　　　　　　原稿ダウンロード実装・UI微調整...大倉" +
+      "<br />" +
+      "- 原稿編集画面：主担当...杉尾" +
+      "<br />" +
+      "　　　　　　　原稿ダウンロード実装・UI微調整...大倉" +
+      "<br />" +
+      "- 発表練習画面：主担当...杉崎" +
+      "<br />" +
+      "　　　　　　　　フルスクリーン表示実装・自動スクロール実装・" +
+      "<br />" +
+      "　　　　　　　　強調表示実装・UI調整...大倉" +
+      "<br />" +
+      "- Firebase構築・管理：大倉 - GitHub構築・管理：大倉" +
+      "<br />" +
+      "- GitHub使用：大倉、杉尾、杉崎",
+    showConfirmButton: true,
+    confirmButtonText: "閉じる",
+    confirmButtonColor: "#1AA7C5",
+  });
+});
+credit_button.addEventListener("click", () => {
+  Swal.fire({
+    title: "作業担当",
+    html:
+      "【企画・立案】大倉、杉尾、杉崎" +
+      "<br /><br />" +
+      "【UIデザイン】<br />主担当...杉尾" +
+      "<br />" +
+      "原稿ダウンロード・全画面表示ボタン・マイク停止ボタン・初期化ボタン・クレジットボタン...大倉" +
+      "<br /><br />" +
+      "【ログイン・アカウント登録画面】大倉" +
+      "<br /><br />" +
+      "【メニュー画面】大倉" +
+      "<br /><br />" +
+      "【原稿リスト画面(新規作成・編集用/発表練習用)】大倉" +
+      "<br /><br />" +
+      "【原稿新規作成画面】<br />主担当...杉尾" +
+      "<br />" +
+      "原稿ダウンロード実装・UI微調整...大倉" +
+      "<br /><br />" +
+      "【原稿編集画面】<br />主担当...杉尾" +
+      "<br />" +
+      "原稿ダウンロード実装・UI微調整...大倉" +
+      "<br /><br />" +
+      "【発表練習画面】<br />主担当...杉崎" +
+      "<br />" +
+      "全画面表示実装・自動スクロール実装・強調表示実装・UI調整...大倉" +
+      "<br /><br />" +
+      "【Firebase構築・管理】大倉" +
+      "<br /><br />" +
+      "【GitHub構築・管理】大倉" +
+      "<br /><br />" +
+      "【GitHub使用】大倉、杉尾、杉崎",
+    showConfirmButton: true,
+    confirmButtonText: "閉じる",
+    confirmButtonColor: "#1AA7C5",
+  });
 });
--- public/js/index_jquery.js
+++ public/js/index_jquery.js
@@ -9,3 +9,16 @@ $("#user-wrap p").click(function () {
   $(".openbtn1").removeClass("active"); //ボタンの activeクラスを除去し
   $("#user-wrap").removeClass("panelactive"); //ナビゲーションのpanelactiveクラスも除去
 });
+
+/* 作業担当 */
+$(".credit").click(function () {
+  //ボタンがクリックされたら
+  $(this).toggleClass("active"); //ボタン自身に activeクラスを付与し
+  $("#modal-01").toggleClass("panelactive"); //ナビゲーションにpanelactiveクラスを付与
+});
+
+$("#user-wrap p").click(function () {
+  //ナビゲーションのリンクがクリックされたら
+  $(".openbtn1").removeClass("active"); //ボタンの activeクラスを除去し
+  $("#user-wrap").removeClass("panelactive"); //ナビゲーションのpanelactiveクラスも除去
+});
